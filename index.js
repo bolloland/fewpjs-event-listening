f66064af134b8db76060b8fee94ef610f5d68efe
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-08-03 13:02:15 -0500

    commit tree c84d50371e0718d4fe25a017d66153e122274c1d

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-function addingEventListener() {
-  const input = document.getElementById('input');
-  input.addEventListener('click', function(event) {
-    alert('I was clicked!');
-  }); 
+const input = document.querySelector("#input")
+const fifth =   document.querySelector("body > div:nth-child(5) > div > div > div > div")
+
+const redBlue = () => {
+  let style = document.querySelector("head > style")
+  let newStyle = style.innerHTML.replace("red", "blue")
+  style.innerText = newStyle
 }
+
+input.addingEventListener('click', redBlue)
+
+fifth.addingEventListener('click', () => {
+  fifth.innerText = "Courtney I Love"
+})
+
+fifth.addingEventListener('mouseover', () => {
+  fifth.innerText = "Courtney I Love"
+})
